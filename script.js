5a51e9ebe42383e9befbc32e18cb98c017fe6bd6
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-04-10 21:00:40 +0545

    initial task for form handling

--- script.js
+++ script.js
@@ -1,37 +1,19 @@
-const firstNumber = 10
-const secondNumber = 20
+// select password1 input and assign to passwordOne variable
+// select password2 input and assign to passwordTwo variable
 
-// If (Conditional) statement in javascript
-if (firstNumber > secondNumber) {
-  console.log('First number is greater than second number!')
-} else if (firstNumber < secondNumber) {
-  console.log('First number is less than second number!')
-}
+// select toggle span and assign to toggle variable
+// select toggle2 input and assign to toggleTwo variable
 
-// if() {
+// Attach event listener to toggle and toggleTwo
+const toggle = document.querySelector('.toggle')
 
-// } else if() {
+// When we click on eye Button, we need to able to see password
 
-// }
+// click, onchange
+toggle.addEventListener('click', function () {
+  console.log('I am clicked!')
+})
+// callback function
+// A function without name: Anonymous function
 
-if (0) {
-  console.log('Running')
-}
-
-// Falsy values
-// 0, '', null, undefined, NaN, false
-
-// Function
-function add(a, b) {
-  return a + b
-}
-
-// Call a function
-const result = add(100, 49)
-const output = add(14, 15)
-// console.log(output)
-
-// Iteration
-for (let i = 1; i <= 10000; i = i + 2) {
-  console.log(i)
-}
+// console.log(toggle)
