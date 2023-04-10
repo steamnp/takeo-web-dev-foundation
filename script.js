49fee9ade803c328b818b64dc9e7b77bf1d70843
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-04-10 21:54:08 +0545

    array and object destructuring

--- script.js
+++ script.js
@@ -1,47 +1,25 @@
 const numbers = [2, 1, 3, 4, 5]
-console.log(numbers[4])
-// We are trying to access element 4 from array block scope
 
-const numThree = 10
+// const firstElement = number[0]
+// const secondElement = number[1]
 
-// every value inside array are called 'element'
-// In Array each and every element are give index number: index number starts from 0
+// useState in React: Reaview this concept
+// Array Descturcuting
+const [firstElement, secondElement] = numbers
 
 const numbersObj = {
   numTwo: 2,
   numOne: 1,
   numThree: 3,
 }
-// every value inside object are called value
-// In object each and every property are give name: property name
 
-console.log(numThree)
-// We are trying to access numThree from global scope
+// Object Descturcuting
+const { numOne, numThree } = numbersObj
 
-console.log(numbersObj.numThree)
-// We are trying to access numThree from obj block scope
+// console.log(numOne)
 
-if (true) {
-  // Block scope
-  const myName = 'Gorakh'
+console.log(...numbers)
 
-  // Accessing global scope in block scope: Permitted
-  console.log(numThree)
-}
-
-// we are console logging in global scope
-// console.log(myName)
-
-// accessing block scope from global scope in not permitted
-
-function print() {
-  const age = 30
-  // Function Scope
-  console.log(numThree)
-}
-
-if (true) {
-  console.log(age)
-}
-
-print()
+// let arr = [5, 1, 8, 16]
+// arr.sort()
+// console.log(arr)
