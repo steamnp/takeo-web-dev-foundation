4397d739d5ce59b5a61d0023bcd949693fb4e1ea
JAYA PRAKASH SHRESTHA <jayaprakashshrestha15@gmail.com>
2023-04-09 23:15:19 -0500

    cssgrid done

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = "Nepal";
+const secondContainer = 100.1;
+const thirdConatiner = ["nepal", 30];
+const fourthContainer = {
+  countryName: "Nepal",
+  population: 30,
+};
+
+console.log(container);
+console.log(secondContainer);
+console.log(thirdContainer);
+console.log(fourthContainer);
