7e2b0855a45e76a77b180fe03991866474ffe6c0
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-04-11 20:59:30 +0545

    rest and spread

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+// Spread Operator/Rest Parameter
+
+const arr = [1, 2, 3, 4, 5, 6, 7]
 // Spread Operator
+// console.log(...arr)
 
-const arr = [1, 2, 3, 4, 5, 6, 7];
-console.log(...arr);
+const countryNames = ['USA', 'Nepal', 'India', 'China']
 
-const countryNames = ["USA", "Nepal", "India", "China"];
+// Rest Operator (parameter)
+const [firstCountry, , ...restCountries] = countryNames
+// console.log(restCountries)
+const [, numberOne, numberTwo, ...remaining] = arr
+// console.log(numberOne)
+// console.log(numberTwo)
+// console.log(remaining)
 
-const [firstCountry, , ...restCountries] = countryNames;
-console.log(restCountries);
-const [, numberOne, numberTwo, ...remaining] = arr;
-console.log(numberOne);
-console.log(numberTwo);
-console.log(remaining);
+// Rest Operator (parameter)
+function print(x, ...a) {
+  console.log(x)
+  console.log(a)
+}
+
+// Spread Operator
+print(...arr)
