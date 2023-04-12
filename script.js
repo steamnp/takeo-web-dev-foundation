1ba39fe0b176a805476a07b744aaff707ea394a8
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-04-12 23:03:21 +0545

    initial code for quote generator

--- script.js
+++ script.js
@@ -1,32 +1,13 @@
-// Promise
-console.log('before promise')
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
 
-// then method -> 2015 (ES6)
-// const result = fetch('https://restcountries.com/v3.1/all')
-//   .then((response) => {
-//     return response.json()
-//   })
-//   .then((countryData) => {
-//     console.log(countryData)
-//   })
+let apiQuotes = []
 
-// Handling (getting data) from promise
-// 1. then method
-// 2. async await
+// Use async await to get quote data
+// update apiQuotes with your quote data
 
-// async await -> 2017 (ES8)
-async function getCountry() {
-  try {
-    console.log('Inside country function')
-    const response = await fetch('https://restcountries.com/v3.1/all')
-    const data = await response.json()
-
-    console.log(data)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-getCountry()
-
-console.log('after promise')
+// https://type.fit/api/quotes
