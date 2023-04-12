43b8c1fab525eb8446d6151ff2fda31267ca3900
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-04-12 21:00:53 +0545

    linear time complexity

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-// Constant Time Complexity O(1)
+// Linear Time Complexity O(n)
 const start = performance.now()
-const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 100]
-console.log(myArray[19])
+const myArray = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4,
+  5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+]
+for (let i = 0; i < myArray.length; i++) {
+  console.log('Running Loop')
+}
 const end = performance.now()
 console.log(`${end - start}`)
