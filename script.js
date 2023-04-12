4014c3fe849bd1154aa29c975b1dacd4bc1e5fbe
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-04-12 21:18:52 +0545

    exponential time complexity

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-// Linear Time Complexity O(n)
+// Exponential Time Complexity O(2^n)
 const start = performance.now()
-const myArray = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4,
-  5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-]
-for (let i = 0; i < myArray.length; i++) {
-  console.log('Running Loop')
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2]
+
+function generateSubsets(array) {
+  if (array.length === 0) {
+    return [[]]
+  }
+  const firstElement = array[0]
+  const remainingElements = array.slice(1)
+  const subsetsWithoutFirst = generateSubsets(remainingElements)
+  const subsetsWithFirst = subsetsWithoutFirst.map((subset) => [firstElement, ...subset])
+  return subsetsWithoutFirst.concat(subsetsWithFirst)
 }
+
+generateSubsets(myArray)
+
 const end = performance.now()
 console.log(`${end - start}`)
