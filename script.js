664e6f8dcde6cd23fb2d3799479ae12ee3370548
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-04-12 20:57:31 +0545

    constant time complexity

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-const name = 'Gorakh'
-
-// const start = performance.now()
-// console.log(start)
-
-// Liner Time Complexity O(n)
-// for (let i = 0; i < 10000; i++) {
-//   console.log('Running Loop')
-// }
-
-// const end = performance.now()
-// console.log(end)
-
-// // Template Literal
-// console.log(`${end - start}`)
-
-// 100 -> 1
-// 1000 -> 19
-// 10000 -> 106
-
 // Constant Time Complexity O(1)
 const start = performance.now()
-console.log(100000000000)
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 100]
+console.log(myArray[19])
 const end = performance.now()
-console.log(end)
+console.log(`${end - start}`)
