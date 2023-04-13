6dd5eba78893e0295abec34da9da54405d440f7e
sushilkhanal <khanal.sushil47@gmail.com>
2023-04-13 08:29:05 -0500

    code as of standup

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
-const toggle = document.querySelector(".toggle");
-const input = document.querySelector("#passwordOne");
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.querySelector(".toggle");
+  const input = document.querySelector("#passwordOne");
 
-toggle.addEventListener("click", function () {
-  if (input.type === "password") {
-    input.type = "text";
-    toggle.classList.remove("fa-eye");
-    toggle.classList.add("fa-eye-slash");
-  } else {
-    input.type = "password";
-    toggle.classList.remove("fa-eye-slash");
-    toggle.classList.add("fa-eye");
-  }
-});
+  toggle.addEventListener("click", function () {
+    if (input.type === "password") {
+      input.type = "text";
+      toggle.classList.remove("fa-eye");
+      toggle.classList.add("fa-eye-slash");
+    } else {
+      input.type = "password";
+      toggle.classList.remove("fa-eye-slash");
+      toggle.classList.add("fa-eye");
+    }
+  });
 
-const toggle2 = document.querySelector(".toggle2");
-const input2 = document.querySelector("#passwordTwo");
-toggle2.addEventListener("click", function () {
-  if (input2.type === "password") {
-    input2.type = "text";
-  } else {
-    input2.type = "password";
-  }
+  const toggle2 = document.querySelector(".toggle2");
+  const input2 = document.querySelector("#passwordTwo");
+  toggle2.addEventListener("click", function () {
+    if (input2.type === "password") {
+      input2.type = "text";
+    } else {
+      input2.type = "password";
+    }
+  });
+
+  const submitBtn = document.querySelector(".input-btn");
+
+  submitBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    const passwordOne = document.getElementById("passwordOne");
+    const passwordTwo = document.getElementById("passwordTwo");
+    const alertBox = document.querySelector(".alert-box");
+    console.log(passwordOne.value);
+    console.log(passwordTwo.value);
+
+    if (passwordOne.value !== passwordTwo.value) {
+      alertBox.innerHTML = "<p>Passwords do not match</p>";
+    } else {
+      alertBox.innerHTML = "<p>Passwords match</p>";
+    }
+  });
 });
