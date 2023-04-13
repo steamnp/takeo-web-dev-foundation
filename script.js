48de50218490b45d1d49cb9b326f5ace9c4c5fe7
Sanjay L <sanjayluitel2@gmail.com>
2023-04-13 09:25:33 -0400

    Quote generator and Twitter Complete

--- script.js
+++ script.js
@@ -7,20 +7,25 @@ const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
-// Use async await to get quote data
-// update apiQuotes with your quote data
-
-// https://type.fit/api/quotes
-// newQuoteBtn.addEventListener("click", getQuote);
-
-// function getElementById() {
-//   fetch("https://type.fit/api/quotes");
-// }
+//Generating a random number between 1 and 1643
 
 async function getQuote() {
   const response = await fetch("https://type.fit/api/quotes");
   const data = await response.json();
   apiQuotes = data;
-  console.log(apiQuotes);
+  //console.log(apiQuotes);
 }
+newQuoteBtn.addEventListener("click", function () {
+  //Generating a random number
+  const randNum = Math.ceil(Math.random() * 1643);
+  authorText.textContent = apiQuotes[randNum].author;
+  quoteText.textContent = apiQuotes[randNum].text;
+});
+
+twitterBtn.addEventListener("click", function () {
+  const randNum = Math.ceil(Math.random() * 1643);
+  output = apiQuotes[randNum].text + " -" + apiQuotes[randNum].author;
+  window.open("https://twitter.com/intent/tweet?text=" + output);
+});
+
 getQuote();
