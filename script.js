2a861c9e039de0f21743700aaf41280ddb9cd8ae
sushilkhanal <khanal.sushil47@gmail.com>
2023-04-12 23:14:25 -0500

    Working quote generator liked with twitter

--- script.js
+++ script.js
@@ -7,14 +7,57 @@ const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
-// Use async await to get quote data
-// update apiQuotes with your quote data
-// https://type.fit/api/quotes
-
 async function getQuote() {
   const response = await fetch("https://type.fit/api/quotes");
   const data = await response.json();
   apiQuotes = data;
-  console.log(apiQuotes);
+  showLoadingSpinner();
+  setTimeout(() => {
+    getRandomQuote();
+    hideLoadingSpinner();
+  }, 1000);
 }
+
+function getRandomQuote() {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // Check if Author field is blank and replace it with "Unknown"
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+}
+
+// Tweet a quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Show loading spinner
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading spinner
+function hideLoadingSpinner() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
+// Event listeners
+newQuoteBtn.addEventListener("click", getRandomQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+// On load
 getQuote();
