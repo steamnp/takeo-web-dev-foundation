7848e5b612622c5731d5305e1b1a6e5be5df08e2
Sanjay L <sanjayluitel2@gmail.com>
2023-04-13 11:24:21 -0400

    Complete Asssignment Partial

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-// Spread Operator
-
-const arr = [1, 2, 3, 4, 5, 6, 7];
-console.log(...arr);
-
-// Rest Operator
-// will be used with descturturing and function parameter
-
-// const [firstNumber] = arr
-// console.log(firstNumber)
-
-const countryNames = ["USA", "Nepal", "India", "China"];
-
-const [firstCountry, , ...restCountries] = countryNames;
-
-console.log(restCountries);
-const [, numberOne, numberTwo, ...remaining] = arr;
-
-console.log(numberOne);
-console.log(numberTwo);
-console.log(remaining);
+var toggle = document.querySelector(".toggle");
+var passOne = document.getElementById("passwordOne");
+var toggleTwo = document.querySelector(".toggle2");
+var passTwo = document.getElementById("passwordTwo");
+var submitBtn = document.querySelector(".input-btn");
+var output = document.querySelector(".alert-box");
+//console.log(toggle);
+toggle.addEventListener("click", function (fun) {
+    if (passOne.type === "password") {
+        passOne.type = "text";
+        toggle.classList.add("fa-eye-slash");
+        toggle.classList.remove("fa-eye");
+    }
+    else {
+        passOne.type = "password";
+    }
+});
+toggleTwo.addEventListener("click", function () {
+    if (passTwo.type === "password") {
+        passTwo.type = "text";
+        toggleTwo.classList.add("fa-eye-slash");
+        toggleTwo.classList.remove("fa-eye");
+    }
+    else {
+        passTwo.type = "password";
+    }
+});
+submitBtn.addEventListener("click", function (fun) {
+    if (passOne.value === passTwo.value) {
+        alert("success");
+    }
+    else {
+        alert("Passwords do not match");
+    }
+});
