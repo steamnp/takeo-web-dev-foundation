890d9ec98788c3245e96b01ceab0cc395837e651
Kenneth Ewonde <Kennethewonde@takeo.ai>
2023-04-16 13:39:13 -0500

    create random quote selector for assignment 15

--- script.js
+++ script.js
@@ -1,38 +1,58 @@
-const quoteContainer = document.getElementById("quote-container");
-const quoteText = document.getElementById("quote");
-const authorText = document.getElementById("author");
-const twitterBtn = document.getElementById("twitter");
-const newQuoteBtn = document.getElementById("new-quote");
-const loader = document.getElementById("loader");
-
-let apiQuotes = [];
-
-// Use async await to get quote data
-// update apiQuotes with your quote data
-
-// https://type.fit/api/quotes
-
-// If you are using async await to consume promises -> use try catch block to handle errors
-// If you are using then method -> use catch function (method) at the end of last then method to handle errors
-
-// method -> function
-const data = fetch('https://type.fit/api/quotes')
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    console.log(data)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
-
-// fetch -> axios
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+// const loader = document.getElementById('.loader');
 
 async function getQuotes() {
-  const response = await fetch("https://type.fit/api/quotes");
-  const data = await response.json();
-  apiQuotes = data;
+  try {
+    const response = await fetch("https://type.fit/api/quotes");
+    const data = await response.json();
+    const apiQuotes = data;
+return apiQuotes;
+  } 
+  catch (error) {
+    console.log(error);
+  }
+}
+
+function displayapiQuotes(apiQuotes) {
+  const randomIndex = Math.floor(Math.random() * apiQuotes.length);
+  const quote = apiQuotes[randomIndex];
+  quoteText.textContent = quote.text;
+  authorText.textContent = quote.author;
   console.log(apiQuotes)
 }
+
+newQuoteBtn.addEventListener("click", async () => {
+  try {
+    const apiQuotes = await getQuotes();
+    displayapiQuotes(apiQuotes);
+  }
+  catch(error) {
+    console.error(error);
+  }
+});
+
+
+twitterBtn.addEventListener("click", async () => {
+  try {
+    const apiQuotes = await getQuotes();
+    displayapiQuotes(apiQuotes);
+  }
+  catch(error) {
+    console.error(error);
+  }
+  console.log()
+});
+
 getQuotes()
+.then((apiQuotes) => {
+  displayapiQuotes(apiQuotes);
+  quoteContainer.style.visibility = 'visible';
+  console.log()
+})
+
+.catch(error => {console.error(error)});
+
