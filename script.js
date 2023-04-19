687c1edc8c67bd85f632a937b94dc372f55e7a27
Sanjay <sanjayluitel2@gmail.com>
2023-04-19 09:30:42 -0400

    Complete Form Validator

--- script.js
+++ script.js
@@ -6,30 +6,31 @@ var submitBtn = document.querySelector(".input-btn");
 var output = document.querySelector(".alert-box");
 //console.log(toggle);
 toggle.addEventListener("click", function (fun) {
-    if (passOne.type === "password") {
-        passOne.type = "text";
-        toggle.classList.add("fa-eye-slash");
-        toggle.classList.remove("fa-eye");
-    }
-    else {
-        passOne.type = "password";
-    }
+  if (passOne.type === "password") {
+    passOne.type = "text";
+    toggle.classList.add("fa-eye-slash");
+    toggle.classList.remove("fa-eye");
+  } else {
+    passOne.type = "password";
+  }
 });
 toggleTwo.addEventListener("click", function () {
-    if (passTwo.type === "password") {
-        passTwo.type = "text";
-        toggleTwo.classList.add("fa-eye-slash");
-        toggleTwo.classList.remove("fa-eye");
-    }
-    else {
-        passTwo.type = "password";
-    }
+  if (passTwo.type === "password") {
+    passTwo.type = "text";
+    toggleTwo.classList.add("fa-eye-slash");
+    toggleTwo.classList.remove("fa-eye");
+  } else {
+    passTwo.type = "password";
+  }
 });
+//const output = document.querySelector(".alert-box");
 submitBtn.addEventListener("click", function (fun) {
-    if (passOne.value === passTwo.value) {
-        alert("success");
-    }
-    else {
-        alert("Passwords do not match");
-    }
+  output.innerHTML = "";
+  const p = document.createElement("p");
+  if (passOne.value === passTwo.value) {
+    p.innerHTML = "Success";
+  } else {
+    p.innerHTML = "Passwords do not match";
+  }
+  output.appendChild(p);
 });
