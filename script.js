89d304dd71f09a218772a938987e0666c7177371
sushilkhanal <khanal.sushil47@gmail.com>
2023-04-18 23:05:15 -0500

    modified code still not getting the required output

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const toggle = document.querySelector(".toggle");
   const input = document.querySelector("#passwordOne");
+  const toggle2 = document.querySelector(".toggle2");
+  const input2 = document.querySelector("#passwordTwo");
+  const submitBtn = document.querySelector(".input-btn");
+  const alertBox = document.querySelector(".alert-box");
 
   toggle.addEventListener("click", function () {
     if (input.type === "password") {
@@ -14,30 +18,36 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  const toggle2 = document.querySelector(".toggle2");
-  const input2 = document.querySelector("#passwordTwo");
   toggle2.addEventListener("click", function () {
     if (input2.type === "password") {
       input2.type = "text";
+      toggle2.classList.remove("fa-eye");
+      toggle2.classList.add("fa-eye-slash");
     } else {
       input2.type = "password";
+      toggle2.classList.remove("fa-eye-slash");
+      toggle2.classList.add("fa-eye");
     }
   });
 
-  const submitBtn = document.querySelector(".input-btn");
+  input.addEventListener("input", validatePasswords);
+  input2.addEventListener("input", validatePasswords);
 
-  submitBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    const passwordOne = document.getElementById("passwordOne");
-    const passwordTwo = document.getElementById("passwordTwo");
-    const alertBox = document.querySelector(".alert-box");
-    console.log(passwordOne.value);
-    console.log(passwordTwo.value);
-
-    if (passwordOne.value !== passwordTwo.value) {
+  function validatePasswords() {
+    if (input.value !== input2.value) {
       alertBox.innerHTML = "<p>Passwords do not match</p>";
+      submitBtn.disabled = true;
     } else {
       alertBox.innerHTML = "<p>Passwords match</p>";
+      submitBtn.disabled = false;
+    }
+  }
+
+  // Prevent form submission if passwords don't match
+  submitBtn.addEventListener("click", function (event) {
+    if (input.value !== input2.value) {
+      event.preventDefault();
+      alertBox.innerHTML = "<p>Please recheck your passwords</p>";
     }
   });
 });
